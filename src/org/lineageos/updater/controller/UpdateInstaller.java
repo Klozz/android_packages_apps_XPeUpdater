4b18bdb6dc0c1cde445a08cc1d521f5bb978f84b
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-07 19:02:37 +0200

    Updater: Move to AndroidX
    Change-Id: I7bd67915638db5330b854f66f9fc1adb864b64be
    Signed-off-by: klozz <klozz@vivaldi.net>

--- src/org/lineageos/updater/controller/UpdateInstaller.java
+++ src/org/lineageos/updater/controller/UpdateInstaller.java
@@ -19,8 +19,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.os.SystemProperties;
-import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.updater.misc.Constants;
 import org.lineageos.updater.misc.FileUtils;
